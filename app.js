4317e481577925299339c1c4538b7cbc65fd7095
dox369 <x_dox_x@hotmail.com>
2022-09-06 15:18:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,17 @@ const path = require('path');
  
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+
+
+.use('/static', express.static(path.join(__dirname, "static")))
+ .use('/socket.io', express.static(path.join(__dirname, "socket.io")))
+ .use('/javascripts', express.static(path.join(__dirname, "javascripts")))
+ .use('/stylesheets', express.static(path.join(__dirname, "stylesheets")))
+ 
+
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
-server.use('/static', express.static(path.join(__dirname, "static")));
- server.use('/socket.io', express.static(path.join(__dirname, "socket.io")));
- server.use('/javascripts', express.static(path.join(__dirname, "javascripts")));
-server.use('/stylesheets', express.static(path.join(__dirname, "stylesheets")));
- 
 
 const app = express()
 
