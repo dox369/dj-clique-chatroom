ff304a9b99536c77e9e6b4a11be0c1a17ec2f2fa
dox369 <x_dox_x@hotmail.com>
2022-09-06 00:23:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -280,7 +280,7 @@ io.on('connection', function(socket){
 
 
 // start the server listening for requests
-app.listen(process.env.PORT || 80, 
+app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
 
 
