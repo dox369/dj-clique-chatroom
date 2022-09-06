5543cb7702e7e3df73b6a4c478710995961d9605
dox369 <x_dox_x@hotmail.com>
2022-09-06 14:53:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
+const path = require('path');
+ 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
