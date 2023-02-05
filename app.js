42d93cf5a7ae70214e267131655977666cef58d9
dox369 <x_dox_x@hotmail.com>
2023-02-05 01:31:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 //========================================================================================
 //========================================================================================
 //========================================================================================
+
+'use strict';
+const express = require('express');
+const socketIO = require('socket.io');
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const io = require("socket.io")(server,{
+  cors: {
+    origins: "*:*",
+    methods: ["GET", "POST"]
+  }
+});
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('messaged', (args) => {
+    io.emit('message', args);
+    console.log(args)
+  });
+
 //========================================================================================
 
+
+/*
 var express = require('express')
 
   , app = express()
@@ -11,16 +35,19 @@ var express = require('express')
 
   , server = http.createServer(app)
 
-  //, io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server);
 
 //var io = io("socket-lb02.onrender.com", {transports: ['websocket']});
 
 
-const io = require('socket.io');
+//const io = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 //server.listen(PORT);
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+*/
+
 
 
 // routing
